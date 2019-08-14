1fdd46c9f1b32cd0b59ddbbfa2b90a187d0bc8af
Paras <paras.vasishth@gmail.com>
2019-08-14 12:16:50 +0530

    Initial commit

---
+++ scrap.py
@@ -0,0 +1,6 @@
+pro = "\nTell me something, and i will repeat it back to you:"
+pro += "\nEnter 'quit' to end the program."
+message = ""
+while message != 'quit':
+    message = input(pro)
+    print(message)
